c2e3dc8d7cae6c08ab7a04f61f231932d0bec75c
Evgeniy <evgeniy_mq@mail.com>
2023-01-17 13:12:35 +0300

    n_1 complited

---
+++ n_1/Program.cs
@@ -0,0 +1,54 @@
+﻿// адача HARD SORT необязательная. Считается за три обязательных
+// Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 10 3
+// После сортировки
+// 1 2 3 4
+// 5 7 9 10
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-99, 100);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    int temp;
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int i2 = 0; i2 < array.GetLength(0); i2++)
+                for (int j2 = 0; j2 < array.GetLength(1); j2++)
+                    if (array[i2, j2] > array[i, j])
+                    {
+                        temp = array[i, j];
+                        array[i, j] = array[i2, j2];
+                        array[i2, j2] = temp;
+                    }
+}
+
+
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SortArray(array);
+PrintArray(array);
